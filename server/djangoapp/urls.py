9dab81aee0d50063b34e836c01382535f4bdc7cf
Raekwill15 <Raekwill15@gmail.com>
2024-01-11 21:05:52 -0500

    User management/authentication complete

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
-
+    path(route='signup/', view=views.registration_request, name='signup'),
     # path for login
     path(route="login/", view=views.login_request, name='login'),
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -62,6 +62,28 @@ def logout_request(request):
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html')
+    elif request.method == "POST":
+        username = request.POST['username']
+        new_user=True
+        try:
+            User.objects.get(username=username)
+            print("______________________")
+            new_user=False
+        except:
+            logger.debug("{} is new user".format(username))
+        if new_user:
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            pswd = request.POST['pswd']
+            user = User.objects.create_user(username=username,first_name=fname,last_name=lname,password=pswd)
+            login(request,user)
+            return redirect('djangoapp:index')
+        else:
+            return redirect('djangoapp:index')
+        # username = request.POST['username']
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
