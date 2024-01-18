ddf756dc4c149938604d18e53c019580cecbda33
Raekwill15 <Raekwill15@gmail.com>
2024-01-18 17:38:56 -0500

    django models and get-dealership function

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
+class carModelInline(admin.StackedInline):
+    model=CarModel
+    extra=1
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['make', 'name', 'dealer_id', 'car_type', 'year']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines=[carModelInline]
+    
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30,) 
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Name: {self.name}'
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,7 +25,25 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    CAR_TYPE = [
+        ("Sedan" , "Sedan"),
+        ("SUV" , "SUV"),
+        ("Wagon", "Wagon"),
+        ("Minivan", "Minivan"),
+        ("Coupe" , "Coupe"),
+        ("Sports Car" , "Sports Car"),
+        ("Truck" , "Truck")
+    ]
 
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    dealer_id = models.IntegerField()
+    car_type = models.CharField(max_length=10, choices=CAR_TYPE)
+    year = models.DateField()
+
+    def __str__(self):
+        return f'Name: {self.name} Car Type: {self.car_type} Year: {self.year}'
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
