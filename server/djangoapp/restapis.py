39d4114cf32cbad92fe9f2e90c5de9665ddecfaf
Raekwill15 <Raekwill15@gmail.com>
2024-01-24 20:11:22 -0500

    fixed api_key bug

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -8,12 +8,12 @@ from requests.auth import HTTPBasicAuth
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-def get_request(url, api_key,**kwargs):
+def get_request(url, api_key=None, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
     try:
         print("__________BEFORE CRASH!!!!_________")
-        if api_key:
+        if api_key != None:
             print("__________BEFORE CRASH!!!!____w/ apikey_____")
             response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs, auth=HTTPBasicAuth('apikey', api_key))
         else:
@@ -102,7 +102,7 @@ def get_dealers_by_state_from_cf(url, dealerState):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
 def get_dealer_reviews_from_cf(url, dealerId):
-    reviews = get_request(url, id = dealerId, api_key=None)
+    reviews = get_request(url, id = dealerId)
     reviews_list = []
     if reviews:
         for x in reviews:
@@ -120,8 +120,6 @@ def get_dealer_reviews_from_cf(url, dealerId):
                 review_obj.car_year = x['car_year']
             reviews_list.append(review_obj)
         return reviews_list
-
-
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
